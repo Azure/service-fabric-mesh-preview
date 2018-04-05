ca9f8c3356f9fea79e7eb0d9f0ee51fc7799b973
root <root@MININT-TETDGIQ.redmond.corp.microsoft.com>
2018-04-04 19:34:35 -0700

    fixing sln file references and removing subscription ID.

--- samples/src/quickstart/Sbz/DeployApplicationAzure.ps1
+++ samples/src/quickstart/Sbz/DeployApplicationAzure.ps1
@@ -1,6 +1,6 @@
 ## TO-DO CLI
 
 Login-AzureRmAccount
-Set-AzureRmContext -SubscriptionId "c484c80e-0a6f-4470-86de-697ecee16984"
+Set-AzureRmContext -SubscriptionId ""
 New-AzureRmResourceGroup -Name Pristine -Location eastus
 New-AzureRmResourceGroupDeployment -Name Pristine -ResourceGroupName SbzVotingLinux -TemplateFile .\SbzVotingARM.json
