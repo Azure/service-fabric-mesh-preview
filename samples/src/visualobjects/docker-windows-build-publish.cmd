d5c57e508e8d717f3cb5cf259e02e26fb8baaac5
Vipul Modi <vipulm@microsoft.com>
2018-06-15 22:29:33 -0700

    create docker build files add linux templates

--- samples/src/visualobjects/docker-windows-build-publish.cmd
+++ samples/src/visualobjects/docker-windows-build-publish.cmd
@@ -1,6 +1,6 @@
-docker build . -f web\Dockerfile -t seabreeze/azure-mesh-visualobjects-web:1.0-nanoserver-1709
-docker build . -f worker\Dockerfile -t seabreeze/azure-mesh-visualobjects-worker:1.0-nanoserver-1709
-docker build . -f worker\rotate.Dockerfile -t seabreeze/azure-mesh-visualobjects-worker:1.0-rotate-nanoserver-1709
-docker push seabreeze/azure-mesh-visualobjects-web:1.0-nanoserver-1709
-docker push seabreeze/azure-mesh-visualobjects-worker:1.0-nanoserver-1709
-docker push seabreeze/azure-mesh-visualobjects-worker:1.0-rotate-nanoserver-1709
+docker build . -f web\Dockerfile -t seabreeze/azure-mesh-visualobjects-web:1.1-nanoserver-1709
+docker build . -f worker\Dockerfile -t seabreeze/azure-mesh-visualobjects-worker:1.1-nanoserver-1709
+docker build . -f worker\rotate.Dockerfile -t seabreeze/azure-mesh-visualobjects-worker:1.1-rotate-nanoserver-1709
+docker push seabreeze/azure-mesh-visualobjects-web:1.1-nanoserver-1709
+docker push seabreeze/azure-mesh-visualobjects-worker:1.1-nanoserver-1709
+docker push seabreeze/azure-mesh-visualobjects-worker:1.1-rotate-nanoserver-1709
