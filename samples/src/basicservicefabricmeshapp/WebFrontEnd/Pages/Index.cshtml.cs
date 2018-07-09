5ae7b6f80405d4ef574a0ef8d4a96c6eaf7220fa
Tyler Whitney <Tyler.Whitney@microsoft.com>
2018-07-09 16:49:04 -0700

    update per tool upgrade changes

--- samples/src/basicservicefabricmeshapp/WebFrontEnd/Pages/Index.cshtml.cs
+++ samples/src/basicservicefabricmeshapp/WebFrontEnd/Pages/Index.cshtml.cs
@@ -1,10 +1,10 @@
-﻿using System.Net.Http;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Http;
 
 namespace WebFrontEnd.Pages
 {
