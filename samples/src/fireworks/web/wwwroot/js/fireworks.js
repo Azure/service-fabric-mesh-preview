2d53d17dde0695d1416265b06dd068d059c6e380
yaron2 <schneider.yaron@live.com>
2018-07-15 13:02:32 -0700

    fixed animation pause issue and re-enabled docker build of worker

--- samples/src/fireworks/web/wwwroot/js/fireworks.js
+++ samples/src/fireworks/web/wwwroot/js/fireworks.js
@@ -6,7 +6,7 @@ var Fireworks = function () {
     var isInit = false;
     var rand = function (rMi, rMa) { return ~~((Math.random() * (rMa - rMi + 1)) + rMi); }
     var hitTest = function (x1, y1, w1, h1, x2, y2, w2, h2) { return !(x1 + w1 < x2 || x2 + w2 < x1 || y1 + h1 < y2 || y2 + h2 < y1); };
-    window.requestAnimFrame = function () { return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (a) { window.setTimeout(a, 1E3 / 60) } }();
+    window.requestAnimFrame = function () { return function (a) { window.setTimeout(a, 1E3 / 60) } }();
 
     /*=============================================================================*/
 	/* Initialize
