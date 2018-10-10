8e2ae035ec778a33e2fee33a9b21067d0c247cb5
Ashish Negi <asnegi@microsoft.com>
2018-10-10 14:03:02 +0530

    Fix CMD in Dockerfile and linux code

--- samples/src/azurefiles-volume-verifier/build.cmd
+++ samples/src/azurefiles-volume-verifier/build.cmd
@@ -1,7 +1,5 @@
 pushd win
-xcopy ..\AzureFilesVerifier.py . || goto :error
 docker build -t azurefilesverifier:win -f Dockerfile . || goto :error
-del AzureFilesVerifier.py || goto :error
 popd
 echo "Image successfully created : name : azurefilesverifier:win"
 
--- samples/src/azurefiles-volume-verifier/build.sh
+++ samples/src/azurefiles-volume-verifier/build.sh
@@ -3,8 +3,6 @@ set -e
 # Any subsequent(*) commands which fail will cause the shell script to exit immediately
 
 pushd linux
-cp ../AzureFilesVerifier.py .
 docker build -t azurefilesverifier:linux -f Dockerfile .
-rm AzureFilesVerifier.py
 popd
 echo "Image successfully created : name : azurefilesverifier:linux"
